bf0cf4a3c5e2fc33f2a2f2a48c4cddc8efb54555
himanshu231015 <himanshukumar231015@gmail.com>
2025-05-28 16:56:49 +0530

    Deleted Eclips

--- Eclipse/Hexashop/src/main/java/com/univ/DB/DBConfig.java
+++
@@ -1,8 +0,0 @@
-package com.univ.DB;
-
-public interface DBConfig {
-	String DRIVER = "com.mysql.cj.jdbc.Driver";
-	String URL = "jdbc:mysql://localhost:3306:sait1";
-	String UNM = "root";
-	String PW = "v1430";
-}
